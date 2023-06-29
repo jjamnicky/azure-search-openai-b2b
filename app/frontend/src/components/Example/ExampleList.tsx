de7b8c59f3ba2ed4497f16a983df94431cf1cafc
jjamnicky <137900228+jjamnicky@users.noreply.github.com>
2023-06-29 19:31:14 +0000

    modified:   app/backend/approaches/chatreadretrieveread.py 	modified:   app/frontend/src/components/Example/ExampleList.tsx 	modified:   app/frontend/src/pages/chat/Chat.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Say something about Slovak Telekom...",
+        value: "Say something about Slovak Telekom..."
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Who is a Director for Mass Market in Slovak Telekom?", value: "Who is a Director for Mass Market in Slovak Telekom?" },
+    { text: "What is Magio GO?", value: "What is Magio GO?" }
 ];
 
 interface Props {
